76905f468dc208f56b777baa50a0badcf869f8c9
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-11-28 15:39:19 +0900

    Initial Commit

---
+++ Gaufre/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gaufre
+//
+//  Created by MacBook Pro on 2021/11/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
