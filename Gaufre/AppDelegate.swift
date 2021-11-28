3e57803f99a8dfdd66d3bd6391c18a558b905350
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-11-28 16:46:02 +0900

    Implemented minimum menu bar app temporary

--- Gaufre/AppDelegate.swift
+++ Gaufre/AppDelegate.swift
@@ -6,15 +6,16 @@
 //
 
 import Cocoa
+import SwiftUI
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
+    private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        buildMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,6 +26,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
-
+    // MARK: - Build status bar
+    func buildMenu() {
+        
+        if let statusBarButton = statusItem.button {
+            statusBarButton.image = NSImage(systemSymbolName: "cpu",
+                                            accessibilityDescription: "app icon")
+            statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
+            statusBarButton.image?.isTemplate = true
+            statusBarButton.target = self
+        }
+        
+        // Each core load view
+        let coreInfoMenuItem = NSMenuItem()
+        coreInfoMenuItem.title = "Core Load"
+        
+        let waftersView = NSHostingView(rootView: WafersView())
+        waftersView.frame = NSRect(x: 0, y: 0, width: 300, height: 250)
+        
+        let coreDetailMenuItem = NSMenuItem()
+        coreDetailMenuItem.view = waftersView
+        
+        let coreInfoSubmenu = NSMenu()
+        coreInfoSubmenu.addItem(coreDetailMenuItem)
+        
+        let mainMenu = NSMenu()
+        mainMenu.addItem(coreInfoMenuItem)
+        mainMenu.setSubmenu(coreInfoSubmenu, for: coreInfoMenuItem)
+        
+        // separater
+        mainMenu.addItem(NSMenuItem.separator())
+        
+        // Quit
+        mainMenu.addItem(withTitle: "Quit",
+                         action: #selector(NSApplication.terminate(_:)),
+                         keyEquivalent: "q")
+        
+        statusItem.menu = mainMenu
+    }
 }
 
---
+++ Gaufre/WafersView.swift
@@ -0,0 +1,20 @@
+//
+//  WafersView.swift
+//  Gaufre
+//
+//  Created by MacBook Pro on 2021/11/28.
+//
+
+import SwiftUI
+
+struct WafersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WafersView_Previews: PreviewProvider {
+    static var previews: some View {
+        WafersView()
+    }
+}
